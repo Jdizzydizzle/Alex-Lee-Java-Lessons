59ee26501113326628870ab9fd2c84aca7d01a91
Jdizzydizzle <127226946+Jdizzydizzle@users.noreply.github.com>
2023-03-13 16:45:00 +0000

    Learning about null pointer exceptions. You can't manipulate nothing

---
+++ NullPointerException/src/NullPointer.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class NullPointer {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Enter a number");
+		Scanner scan = new Scanner(System.in);
+		System.out.println(scan.nextInt());
+
+	}
+}
